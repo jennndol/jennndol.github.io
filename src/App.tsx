9bf18e5c426bccb8b8cf7c276c5d3d2402fbc061
Luthfi <jkt.luthfi@gmail.com>
2025-05-21 00:26:12 +0700

    Refactor App component to use Tailwind CSS and remove unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-extrabold text-white">Hello, world!</h1>
     </div>
   );
 }
